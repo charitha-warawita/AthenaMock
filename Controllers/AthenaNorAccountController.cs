9cbcc79e14475194c6f2bce29bf589f852e603ca
Charitha.Waravita <charitha.warawita_hcl@kantar.com>
2023-03-20 12:04:53 +0530

    update controllers..

--- Controllers/AthenaNorAccountController.cs
+++ Controllers/AthenaNorAccountController.cs
@@ -15,10 +15,38 @@ namespace AthenaMock.Controllers
             _logger = logger;
         }
 
+        //[HttpPost]
+        //public AthenaNORAccountResponse GetPrice(AthenaNORAccountRequest request) 
+        //{
+        //    return new AthenaNORAccountResponse();
+        //}
+
         [HttpPost]
-        public AthenaNORAccountResponse GetPrice(AthenaNORAccountRequest request) 
+        public string GetPriceTest(int val)
         {
-            return new AthenaNORAccountResponse();
+            if (val < 100)
+            {
+                return $"{val} is smaller than 100";
+            }
+            return $"{val} is equal or bigger than 100";
+        }
+
+        [HttpPost]
+        public AthenaNORAccountResponse GetPrice(int valueInt)
+        {
+            var res = new AthenaNORAccountResponse();
+            res.project_id = 1233;
+            res.markets = new List<Models.NOR_Account.Response.Market>();
+
+            Models.NOR_Account.Response.Market mkt = new Models.NOR_Account.Response.Market {
+                locale = "en-US",
+                market = "USA",
+                preferred_currency = "EUR",
+                price_total= 2173
+            };
+            mkt.targeted_audiences = new List<Models.NOR_Account.Response.TargetedAudience>();  
+
+            return res;
         }
     }
 }
